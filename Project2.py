fc5cc8565f1fce732de903adb3ec9678a9e74bd6
LanaElliott <134975988+LanaElliott@users.noreply.github.com>
2023-11-26 19:13:43 -0500

    phew
    oh man I almost lost this that would have been bad

--- Project2.py
+++ Project2.py
@@ -6,7 +6,7 @@ import tensorflow as tf
 tf.compat.v1.set_random_seed(2019)
 
 model = tf.keras.models.Sequential([
-    tf.keras.layers.Conv2D(16, (3, 3), activation="relu", input_shape=(180, 180, 3)),
+    tf.keras.layers.Conv2D(16, (3, 3), activation="relu", input_shape=(100, 100, 3)),
     tf.keras.layers.MaxPooling2D(2, 2),
     tf.keras.layers.Conv2D(32, (3, 3), activation="relu"),
     tf.keras.layers.MaxPooling2D(2, 2),
@@ -31,41 +31,103 @@ train_dir = "C:/Users/rmbar/OneDrive/Documents/GitHub/AER850_Project2/Data/Train
 validation_dir = "C:/Users/rmbar/OneDrive/Documents/GitHub/AER850_Project2/Data/Test/"
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
-train_datagen = ImageDataGenerator(rescale=1.0 / 255.)
-test_datagen = ImageDataGenerator(rescale=1.0 / 255.)
+train_datagen = ImageDataGenerator(rescale=1.0 / 255., shear_range=0.2, zoom_range=0.2, horizontal_flip=True)
+validation_datagen = ImageDataGenerator(rescale=1.0 / 255.)
 
-train_generator = train_datagen.flow_from_directory(train_dir, batch_size=bs, class_mode='categorical', target_size=(180, 180))
+train_generator = train_datagen.flow_from_directory(train_dir, batch_size=bs, class_mode='categorical', target_size=(100, 100))
 
-validation_generator = test_datagen.flow_from_directory(validation_dir,
+validation_generator = validation_datagen.flow_from_directory(validation_dir,
                                                         batch_size=bs,
                                                         class_mode='categorical',
-                                                        target_size=(180, 180))
+                                                        target_size=(100, 100))
 # Compile the model:
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
-# Training the model
-history = model.fit(train_generator,
-                    validation_data=validation_generator,
-                    steps_per_epoch=150 // bs,
-                    epochs=40,
-                    validation_steps=50 // bs,
-                    verbose=2)
+#Creating Model 2:
 
-history_second_fit = model.fit(train_generator, validation_data=validation_generator, epochs=30)
+model2 = tf.keras.models.Sequential([
+    tf.keras.layers.Conv2D(128, (3, 3), strides=(1, 1), activation='LeakyReLU', input_shape=(100, 100, 3)),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Conv2D(256, (3, 3), strides=(1, 1), activation='LeakyReLU'),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Conv2D(512, (3, 3), strides=(1, 1), activation='LeakyReLU'),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(1024, activation='LeakyReLU'),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Dense(4, activation='softmax')  # New output layer
+])
 
-# Plotting part 4
-plt.plot(history_second_fit.history['accuracy'])
-plt.plot(history_second_fit.history['val_accuracy'])
-plt.title('Model Accuracy')
+#Compiling the model2:
+model2.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+
+# Define the PrintShapeCallback class
+class PrintShapeCallback(tf.keras.callbacks.Callback):
+    def on_epoch_end(self, epoch, logs=None):
+        y_true, y_pred = self.validation_data[1], self.model.predict(self.validation_data[0])
+        print(f"Shapes: y_true={y_true.shape}, y_pred={y_pred.shape}")
+
+# Training the models
+history1 = model.fit(train_generator,
+                     validation_data=validation_generator,
+                     steps_per_epoch=150 // bs,
+                     epochs=40,
+                     validation_steps=50 // bs,
+                     verbose=2)
+
+# Use the PrintShapeCallback for Model 2
+history2 = model2.fit(train_generator,
+                      validation_data=validation_generator,
+                      steps_per_epoch=150 // bs,
+                      epochs=10,
+                      validation_steps=50 // bs,
+                      verbose=2)
+
+# Plotting part for Model 1
+plt.figure(figsize=(12, 6))
+
+plt.subplot(1, 2, 1)
+plt.plot(history1.history['accuracy'])
+plt.plot(history1.history['val_accuracy'])
+plt.title('Model 1 Accuracy')
 plt.xlabel('Epoch')
 plt.ylabel('Accuracy')
-plt.legend(['Train','Validation'], loc='upper left')
+plt.legend(['Train', 'Validation'], loc='upper left')
 
-
-plt.plot(history_second_fit.history['loss'])
-plt.plot(history_second_fit.history['val_loss'])
-plt.title('Training and Validation Loss')
+plt.subplot(1, 2, 2)
+plt.plot(history1.history['loss'])
+plt.plot(history1.history['val_loss'])
+plt.title('Model 1 Training and Validation Loss')
 plt.xlabel('Epoch')
 plt.ylabel('Loss')
 plt.legend(['Train', 'Validation'], loc='upper left')
+
+plt.tight_layout()
 plt.show()
+
+# Plotting part for Model 2
+plt.figure(figsize=(12, 6))
+
+plt.subplot(1, 2, 1)
+plt.plot(history2.history['accuracy'])
+plt.plot(history2.history['val_accuracy'])
+plt.title('Model 2 Accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.legend(['Train', 'Validation'], loc='upper left')
+
+plt.subplot(1, 2, 2)
+plt.plot(history2.history['loss'])
+plt.plot(history2.history['val_loss'])
+plt.title('Model 2 Training and Validation Loss')
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
+plt.legend(['Train', 'Validation'], loc='upper left')
+
+plt.tight_layout()
+plt.show()
+
+# Saving the models to be accessible for part 5
+model.save('Model_1.h5')
+model2.save('Model_2.h5')
